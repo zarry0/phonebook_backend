991b100e760cc26daab8c93c65365ecfd6f1f252
zarry0 <cs.rgzarrabal@gmail.com>
2024-07-30 11:24:34 -0600

    Added - exercise 3.18 solution

--- index.js
+++ index.js
@@ -17,36 +17,18 @@ morgan.token('post-body', (req, res) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms  :post-body'));
 
 const url = process.env.MONGODB_URI;
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-];
 
 app.get('/info', (req, res) => {
-    const responseInfo = `
-        Phonebook has info for ${persons.length} people
-        <br/>
-        ${new Date()}
-    `;
-    res.send(responseInfo);
+    const accessDate = new Date();
+    Person.find({})
+        .then(result => {
+            const responseInfo = `
+                Phonebook has info for ${result.length} people
+                <br/>
+                ${accessDate}
+            `;
+            res.send(responseInfo);
+        });
 });
 
 app.get('/api/persons', (req, res) => {
@@ -79,11 +61,6 @@ app.post('/api/persons', (req,res) => {
     }
 
     // ignore duplicates at this stage
-    // if (persons.find(p => p.name === body.name)){
-    //     return res.status(400).json({
-    //         error: 'name must be unique'
-    //     });
-    // }
 
     const newPerson = new Person({
         name: body.name,
